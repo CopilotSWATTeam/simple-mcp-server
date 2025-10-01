cba80cbfaf3f5a31f3c6572a19b16d948d8a341f
D. Tyler Durham <tylerd@tmbpsnorkco.local>
2025-10-01 16:55:29 -0500

    Added command line arg for name.

--- src/clients/tool-greet.py
+++ src/clients/tool-greet.py
@@ -1,5 +1,12 @@
 import asyncio
+import sys
+
 from fastmcp import Client
+
+if len(sys.argv) > 1:
+    name = sys.argv[1] 
+else:
+    name = "World"
 
 client = Client("http://localhost:8000/mcp")
 
@@ -8,4 +15,4 @@ async def call_tool(name: str):
         result = await client.call_tool("greet", {"name": name})
         print(result)
 
-asyncio.run(call_tool("Ford"))
+asyncio.run(call_tool(name))
