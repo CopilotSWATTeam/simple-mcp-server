2ee461efd9bc2c3c0c570f4c26e6e98ae6a090b2
D. Tyler Durham <tylerd@tmbpsnorkco.local>
2025-10-01 16:54:36 -0500

    Added ngrok start script.

---
+++ start-ngrok.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+if [[ ! -f ".env" ]]; then
+  echo "ERROR!"
+  echo "Please create an '.env' file. You can use 'sample.env' as a starting point by renaming it to '.env'."
+  exit 1
+fi    
+
+source .env
+ngrok http --url=$NGROK_URL $PORT
